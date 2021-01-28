ac902806ac759106cf1d7d126d43bd6e156fd6e3
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-01-28 16:56:33 +0700

    add session

--- server/app.js
+++ server/app.js
@@ -1,13 +1,33 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const pool = require("./connection");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-//middleware
-app.use(cors());
+const app = express();
 app.use(express.json());
+//middleware
+app.use(
+  cors({
+    origin: ["https://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    key: "userID",
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { expires: 60 * 60 * 24 },
+  })
+);
 
 //Routes
 app.post("/api/register", (req, res) => {
@@ -40,11 +60,13 @@ app.post("/api/login", (req, res) => {
     if (err) {
       return res.send({ message: "something happened" });
     }
-
     if (result.rows.length > 0) {
-      bcrypt.compare(password, result.rows[0].password, (err, result) => {
-        if (result) return res.send(result);
-        else
+      bcrypt.compare(password, result.rows[0].password, (err, response) => {
+        if (response) {
+          req.session.user = result.rows[0];
+          console.log(req.session.user);
+          return res.send(result.rows[0]);
+        } else
           return res.send({ message: "Wrong Username/Password combination" });
       });
     } else {
